24303e2e3e22b5e22018c7de35a74e418a450fed
BreeLatimer <breelatimer@u.boisestate.edu>
2022-12-05 01:03:17 -0700

    Finished day 4

---
+++ Day4/Day4Part2/day4part2.py
@@ -0,0 +1,41 @@
+def main():
+    validPairs = 0
+    
+    # Read the input file
+    with open('input.txt') as f:
+        data = f.read().splitlines()
+
+    for line in data:
+        # Split the line into words
+        ranges = line.split(',')
+
+        range1 = ranges[0].split("-")
+        range2 = ranges[1].split("-")
+        
+        fullList1 = list()
+        fullList2 = list()
+
+        for i in range(int(range1[0]), int(range1[1]) + 1):
+            fullList1.append(i)
+
+        for i in range(int(range2[0]), int(range2[1]) + 1):
+            fullList2.append(i)
+
+        list1containsList2 = False
+        list2containsList1 = False
+
+        for i in fullList1:
+            if i in fullList2:
+                list1containsList2 = True
+
+        for i in fullList2:
+            if i in fullList1:
+                list2containsList1 = True
+
+        if(list1containsList2 or list2containsList1):
+            validPairs += 1
+    
+    print(validPairs)
+
+if __name__ == "__main__":
+    main()
